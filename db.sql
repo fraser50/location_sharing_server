cb1083bdbab39872c5ca4fb210f392b34c0426ed
Fraser Watson <fraser.watson@mac.com>
2023-02-03 11:42:50 +0000

    Improvements to DB - Added foreign keys, PK for groupMembers & creationDate initial values

--- db.sql
+++ db.sql
@@ -12,20 +12,22 @@ CREATE TABLE IF NOT EXISTS groups(
     groupName VARCHAR(64) NOT NULL,
     groupDescription VARCHAR(10000),
     groupColour CHAR(6) DEFAULT 'ffffff',
-    creationDate TIMESTAMP NOT NULL
+    creationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS groupMembers(
-    memberID CHAR(64) NOT NULL UNIQUE,
-    userID CHAR(64) NOT NULL,
+    /*memberID CHAR(64) NOT NULL UNIQUE, Don't think there is any point in giving a user a unique ID in every group */
+    userID CHAR(64) NOT NULL REFERENCES users(userID),
+    groupID CHAR(64) NOT NULL REFERENCES groups(groupID),
     nickname VARCHAR(32) DEFAULT NULL,
-    joinDate TIMESTAMP NOT NULL,
-    lastSeen TIMESTAMP DEFAULT NULL
+    joinDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    lastSeen TIMESTAMP DEFAULT NULL,
+    PRIMARY KEY (userID,groupID)
 );
 
 CREATE TABLE IF NOT EXISTS previousPositions(
     positionID CHAR(64) PRIMARY KEY,
-    userID CHAR(64) NOT NULL,
-    dateRecorded TIMESTAMP NOT NULL,
+    userID CHAR(64) NOT NULL REFERENCES users(userID),
+    dateRecorded TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     recordedPoint POINT NOT NULL
 );
