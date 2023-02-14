57147b0ba1433c0cfb333314dc6cf4cc6fec8eff
Fraser Watson <fraser.watson@mac.com>
2023-02-14 15:02:04 +0000

    authUser and home page now send JSON responses

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const pool = new pg.Pool();
 
 function authUser(req, res, next) {
     // TODO: probably want to return errors in JSON later
-    if (typeof req.cookies.key != "string") return res.status(403).send("No auth key provided!");
+    if (typeof req.cookies.key != "string") return res.send({status: "failure", reason: "Login key was not provided!"});
 
     var authKey = req.cookies.key;
     pool.query("SELECT userID,studyType,lastSeen,defaultName,userType FROM users where authKey=$1::text", [authKey], (err, results) => {
         if (err) return next(err);
 
         if (results.rowCount == 0) {
-            return res.status(403).send("Authentication key is not valid!");
+            return res.send({status: "failure", reason: "Login key was incorrect!"});
 
         } else {
             req.user = results.rows[0];
@@ -31,7 +31,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", authUser, (req, res) => {
-    res.send("User ID: " + req.user.userid);
+    res.send({
+        status: "success",
+        userid: req.user.userid
+    });
 });
 
 app.post("/creategroup", authUser, (req, res, next) => {
